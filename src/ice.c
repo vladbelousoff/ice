eab316934bd3b7d3ff404e93bbbeda0ad44e71bd
Vladislav Belousov <vladbelousoff@inbox.ru>
2022-10-27 18:00:40 +0300

    Simple test for Expr

--- src/ice.c
+++ src/ice.c
@@ -2,7 +2,7 @@
 
 //static const char source[] = "function main() : int { log.debug('Hello World!'); return 0; }";
 //static const char source[] = "variable 100 \n300.1  200  123.75 'Hello'\t_World 'Hello World' 56.";
-static const char source[] = "11 + 32";
+static const char source[] = "11 + 32 * 2.4";
 
 int main(int argc, char* argv[]) {
    iceLexerT lexer;
@@ -11,9 +11,9 @@ int main(int argc, char* argv[]) {
 
    iceAstExprT* expr = iceAstExpr(&lexer);
    if (expr != NULL) {
-      printf("EXPR %d\n", expr->type);
       printf("EXPR %d\n", expr->binOp->lhs->lit->i32);
-      printf("EXPR %d\n", expr->binOp->rhs->lit->i32);
+      printf("EXPR %d\n", expr->binOp->rhs->binOp->lhs->lit->i32);
+      printf("EXPR %f\n", expr->binOp->rhs->binOp->rhs->lit->f32);
       iceMemTerm(expr);
    }
 
